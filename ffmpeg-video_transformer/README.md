e9acab4ad865ba37ce59b570784cb783e2a06289
Troy Williams <twill3645@gmail.com>
2026-09-07 16:30:29 -0500

    Add project files.

---
+++ ffmpeg-video_transformer/README.md
@@ -0,0 +1,1 @@
+# ffmpeg-video_transformer
---
+++ ffmpeg-video_transformer/video_transformer.cpp
@@ -0,0 +1,9 @@
+﻿// video_transformer.cpp : Defines the entry point for the application.
+//
+
+#include "video_transformer.h"
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
